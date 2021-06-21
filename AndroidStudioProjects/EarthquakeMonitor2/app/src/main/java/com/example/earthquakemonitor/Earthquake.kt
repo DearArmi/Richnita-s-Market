1ae210c86278028da56af87a8e0132b89850a9df
Armando Davila <78766089+DearArmi@users.noreply.github.com>
2021-06-21 14:14:01 -0400

    Delete AndroidStudioProjects/EarthquakeMonitor2 directory

--- AndroidStudioProjects/EarthquakeMonitor2/app/src/main/java/com/example/earthquakemonitor/Earthquake.kt
+++
@@ -1,9 +0,0 @@
-package com.example.earthquakemonitor
-
-import android.os.Parcelable
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize @Entity(tableName = "earthqueake")
-data class Earthquake(@PrimaryKey val id:String, val place:String, val magnitude:Double, val time:Long, val longitude:Double, val latitude:Double):Parcelable
--- AndroidStudioProjects/EarthquakeMonitor2/app/src/main/java/com/example/earthquakemonitor/api/ApiResponseStatus.kt
+++
@@ -1,5 +0,0 @@
-package com.example.earthquakemonitor.api
-
-enum class ApiResponseStatus{
-    ERROR, LOADING, DONE
-}
